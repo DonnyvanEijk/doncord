16fd5aaa1ede0c837a8029255723f1f4f65bb12d
Donny van Eijk <donnyvaneijk873@gmail.com>
2023-11-24 15:50:48 +0100

    emojis and file upload to chat

--- components/modals/message-file-modal.tsx
+++ components/modals/message-file-modal.tsx
@@ -1,155 +1,117 @@
-"use client"
+"use client";
 
-import axios from "axios"
-import * as z from "zod"
-import {zodResolver} from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
+import axios from "axios";
+import qs from "query-string";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle
-
-} from "@/components/ui/dialog"
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import {
-Form,
-FormControl,
-FormField,
-FormItem,
-FormLabel,
-FormMessage
-} from '@/components/ui/form'
-import {Input} from '@/components/ui/input'
-import {Button} from '@/components/ui/button'
-import {  useEffect, useState } from "react"
-import { FileUpload } from "@/components/file-upload"
-import { useRouter } from "next/navigation"
-
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { FileUpload } from "@/components/file-upload";
+import { useRouter } from "next/navigation";
+import { useModal } from "@/hooks/use-modal-store";
 
 const formSchema = z.object({
-name: z.string().min(1, {
-    message: "Server name is required"
-}),
-imageUrl: z.string().min(1, {
-    message: "Server icon is required"
-})
-})
-
-
-export const InitialModal = () => {
-    const [isMounted, setIsMounted] = useState(false);
-    const router = useRouter();
-
-    useEffect(() => {
- setIsMounted(true)
-    }, [])
-
-
-const form = useForm({ 
-resolver: zodResolver(formSchema),
-defaultValues: {
-    name: "",
-    imageUrl: ""
-
-}
+  fileUrl: z.string().min(1, {
+    message: "Attachment is required."
+  })
 });
 
-const isLoading = form.formState.isSubmitting;
+export const MessageFileModal = () => {
+  const { isOpen, onClose, type, data } = useModal();
+  const router = useRouter();
 
-const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    try{
-await axios.post("/api/servers", values);
+  const isModalOpen = isOpen && type === "messageFile";
+  const { apiUrl, query } = data;
 
-form.reset();
-router.refresh();
-window.location.reload()
-
-    } catch (error) {
-console.log(error)
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fileUrl: "",
     }
+  });
 
-}
+  const handleClose = () => {
+    form.reset();
+    onClose();
+  }
 
-if (!isMounted) {
-    return null;
-}
+  const isLoading = form.formState.isSubmitting;
 
-return (
-  <Dialog open>
-    <DialogContent className="bg-white text-black p-0 overflow-hidden">
-      <DialogHeader className="pt-8 px-6">
-        <DialogTitle className="text-2xl text-center font-bold">
-          Customize your server
-        </DialogTitle>
-        <DialogDescription className="text-center text-zinc-500">
-          Give your server a name and a image as a icon so your server has some
-          personality. You can always chage this later
-        </DialogDescription>
-      </DialogHeader>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="
-    space-y-8"
-        >
-          <div className="space-y-8 px-6">
-            <div className="flex items-center justify-center text-center">
-              <FormField
-              control = {form.control}
-              name="imageUrl"
-             render={({ field }) => (
-                <FormItem>
-                    <FormControl>
-                       <FileUpload
-                       endpoint="serverImage"
-                       value={field.value}
-                       onChange={field.onChange}
-                       />
-                    </FormControl>
-                </FormItem>
-             )}
-              
-              />
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const url = qs.stringifyUrl({
+        url: apiUrl || "",
+        query,
+      });
 
-             
-             
-            </div>
+      await axios.post(url, {
+        ...values,
+        content: values.fileUrl,
+      });
 
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) =>(
+      form.reset();
+      router.refresh();
+      handleClose();
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-                <FormItem>
-                    <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
-                        Server name
-                     </FormLabel>
-                     <FormControl>
-                        <Input
-                        disabled={isLoading}
-                        className="bg-zinc-300/50 border-0
-                        focus-visible:ring-0 text-black
-                        focus-visible:ring-offset-0"
-                        placeholder="Enter a server name"
-                        {...field}
+  return (
+    <Dialog open={isModalOpen} onOpenChange={handleClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Add an attachment
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Send a file as a message
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-8 px-6">
+              <div className="flex items-center justify-center text-center">
+                <FormField
+                  control={form.control}
+                  name="fileUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="messageFile"
+                          value={field.value}
+                          onChange={field.onChange}
                         />
-                     </FormControl>
-                     <FormMessage/>
-                </FormItem>
-              )}
-            />
-          </div>
-          <DialogFooter className="bg-gray-100 px-6 py-4">
-            <Button variant="primary" disabled={isLoading}>
-                Create
-            </Button>
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            <DialogFooter className="bg-gray-100 px-6 py-4">
+              <Button variant="primary" disabled={isLoading}>
+                Send
+              </Button>
             </DialogFooter>
-        
-        </form>
-      </Form>
-    </DialogContent>
-  </Dialog>
-);
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  )
 }
